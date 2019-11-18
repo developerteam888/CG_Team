3a57138c4c7461f4f598c077379d0d866dfe117b
solution888 <solution888.github.com>
2019-11-18 08:06:00 +0800

    commit master

---
+++ data.js
@@ -0,0 +1,304 @@
+let data = '{ \
+"data":{ \
+	"car_race": {"title":"Car Racing Game", \
+		"comment":"Car Driving and Racing Simulator", \
+		"responsible":"Entire technical part", \
+		"skills":"Unity3D", \
+		"cover":"img/works/car_racing/1.jpg", \
+		"link":"", \
+		"video":"./img/works/car_racing/video.mp4", \
+		"imgs":[ "img/works/car_racing/1.jpg", \
+				"img/works/car_racing/2.jpg", \
+				"img/works/car_racing/3.jpg"] \
+		}, \
+	"animal_race": {"title":"Animal Racing Game", \
+		"comment":"An exciting go-kart simulation game with a twist", \
+		"responsible":"Entire technical part", \
+		"skills":"Unity3D", \
+		"cover":"img/works/animal_racing/1.jpg", \
+		"link":"", \
+		"video":"./img/works/animal_racing/video.mp4", \
+		"imgs":[ "img/works/animal_racing/1.jpg", \
+				"img/works/animal_racing/2.jpg", \
+				"img/works/animal_racing/3.jpg", \
+				"img/works/animal_racing/4.jpg"] \
+		}, \
+	"ski": {"title":"Go for the gold experience", \
+		"comment":"Ski jumping, Speed skating simulation game", \
+		"responsible":"Entire technical part", \
+		"skills":"Unity3D / Kinect", \
+		"cover":"img/works/ski/1.jpg", \
+		"link":"", \
+		"video":"./img/works/ski/video.mp4", \
+		"imgs":[ "img/works/ski/1.jpg", \
+				"img/works/ski/2.jpg", \
+				"img/works/ski/3.jpg", \
+				"img/works/ski/4.jpg"] \
+		}, \
+	"Driving VR": {"title":"Driving VR", \
+		"comment":"We can dirve the car in VR space", \
+		"responsible":"Entire technical part", \
+		"skills":"CG,Unity3D", \
+		"cover":"img/works/unity/VR/VRDriving1.jpg", \
+		"link":"https://photos.app.goo.gl/tbKC9jVjRSsjbtJt9", \
+		"video":"", \
+		"imgs":[ "img/works/unity/VR/VRDriving1.jpg", \
+				"img/works/unity/VR/VRDriving2.jpg", \
+				"img/works/unity/VR/VRDriving3.jpg", \
+				"img/works/unity/VR/VRDriving4.jpg", \
+				"img/works/unity/VR/VRDriving5.jpg"] \
+		},\
+	"weixin Game": {"title":"weixin Casual Game", \
+		"comment":"We can enjoy the casual game", \
+		"responsible":"Entire technical part", \
+		"skills":"Unity3D", \
+		"cover":"img/works/unity/casual/weixingame1.jpg", \
+		"link":"https://photos.app.goo.gl/XmUhKjUi9McT9Dcb9", \
+		"video":"", \
+		"imgs":[ "img/works/unity/casual/weixingame1.jpg", \
+				"img/works/unity/casual/weixingame2.jpg", \
+				"img/works/unity/casual/weixingame3.jpg", \
+				"img/works/unity/casual/weixingame4.jpg", \
+				"img/works/unity/casual/weixingame5.jpg"] \
+		},\
+	"smartpitch": {"title":"SmartPitch", \
+		"comment":"Precision Live Hitting Training Tool", \
+		"responsible":"Core engine that processes camera image feeds using cvBlob and calculate the speed of ball", \
+		"skills":"Objective-C / Java / C++ / Android NDK / cvBlob(OpenCV)", \
+		"cover":"img/works/SmartPitch/5.jpg", \
+		"link":"https://smartpitchbaseball.com", \
+		"video":"", \
+		"imgs":[ "img/works/SmartPitch/0.jpg", \
+				"img/works/SmartPitch/1.jpg", \
+				"img/works/SmartPitch/2.jpg", \
+				"img/works/SmartPitch/3.jpg", \
+				"img/works/SmartPitch/4.jpg", \
+				"img/works/SmartPitch/5.jpg"] \
+		}, \
+	"qditor": {"title":"Qditor", \
+		"comment":"All-in-one Video Editing Tool", \
+		"responsible":"Entire technical part", \
+		"skills":"OpenCV / FFMpeg / OpenGL / GPU", \
+		"cover":"img/works/qditor/qditor_pc/main.jpg", \
+		"link":"", \
+		"video":"./img/works/qditor/video.mp4", \
+		"imgs":[ "img/works/qditor/qditor_pc/1.jpg", \
+				"img/works/qditor/qditor_pc/2.jpg", \
+				"img/works/qditor/qditor_pc/5.jpg", \
+				"img/works/qditor/qditor_pc/7.jpg", \
+				"img/works/qditor/qditor_pc/8.jpg", \
+				"img/works/qditor/qditor_pc/9.jpg", \
+				"img/works/qditor/qditor_mobile/1.jpg", \
+				"img/works/qditor/qditor_mobile/2.jpg", \
+				"img/works/qditor/qditor_mobile/3.jpg", \
+				"img/works/qditor/qditor_mobile/6.jpg"] \
+		}, \
+	"epub": {"title":"Epub3 Solution", \
+		"comment":"All-in-one Epub3 editor and viewer", \
+		"responsible":"Entire technical part", \
+		"skills":"Epub3 / Text Engine / Qt / iOS / Android", \
+		"cover":"img/works/epub/main.jpg", \
+		"link":"https://github.com/developerteam888/epubSolution", \
+		"video":"", \
+		"imgs":[ "img/works/epub/1.jpg", \
+				"img/works/epub/2.jpg", \
+				"img/works/epub/3.jpg", \
+				"img/works/epub/4.jpg", \
+				"img/works/epub/5.jpg", \
+				"img/works/epub/6.jpg"] \
+		}, \
+	"3dwebpage": {"title":"Creative Live", \
+		"comment":"3D visualized landing page", \
+		"responsible":"Entire technical part", \
+		"skills":"WebGL, three.js, Mesh blending, Particle Effect", \
+		"cover":"img/works/3dwebpage/1.jpg", \
+		"link":"", \
+		"video":"", \
+		"imgs":[ "img/works/3dwebpage/1.jpg"] \
+		},\
+	"blackjack": {"title":"3D Poker Game", \
+		"comment":"Poker, Blackjack, Roulette", \
+		"responsible":"Entire technical part", \
+		"skills":"Unity3D", \
+		"cover":"img/works/blackjack/1.jpg", \
+		"link":"", \
+		"video":"./img/works/blackjack/video.mp4", \
+		"imgs":[ "img/works/blackjack/1.jpg",\
+				"img/works/blackjack/2.jpg", \
+				"img/works/blackjack/3.jpg", \
+				"img/works/blackjack/4.jpg", \
+				"img/works/blackjack/5.jpg", \
+				"img/works/blackjack/6.jpg", \
+				"img/works/blackjack/7.jpg"] \
+		},\
+	"jwviewer": {"title":"JW CAD Viewer", \
+		"comment":"Mobile version CAD Viewer for JWW, JWC, and DXF", \
+		"responsible":"Entire technical part", \
+		"skills":"CAD Engine, C++, Objective-C", \
+		"cover":"img/works/jwviewer/1.jpg", \
+		"link":"https://apps.apple.com/jp/app/ijcad-mobile-dwg%E5%9B%B3%E9%9D%A2%E5%AF%BE%E5%BF%9C%E3%81%AEviewer/id651292081", \
+		"video":"", \
+		"imgs":[ "img/works/jwviewer/1.jpg",\
+				"img/works/jwviewer/2.jpg", \
+				"img/works/jwviewer/3.jpg", \
+				"img/works/jwviewer/4.jpg", \
+				"img/works/jwviewer/5.jpg", \
+				"img/works/jwviewer/6.jpg", \
+				"img/works/jwviewer/7.jpg"] \
+		},\
+	"King": {"title":"King Casual Game", \
+		"comment":"We can enjoy the casual game", \
+		"responsible":"Entire technical part", \
+		"skills":"Unity3D", \
+		"cover":"img/works/unity/casual/KingGame1.jpg", \
+		"link":"https://photos.app.goo.gl/i3UbZ2LiVK666Wnu6", \
+		"video":"", \
+		"imgs":[ "img/works/unity/casual/KingGame1.jpg", \
+				"img/works/unity/casual/KingGame.jpg",  \
+				"img/works/unity/casual/KingGame2.jpg", \
+				"img/works/unity/casual/KingGame3.jpg"] \
+		},\
+	"arbasketball": {"title":"AR Basketball", \
+		"comment":"AR basketball simulation app", \
+		"responsible":"Entire technical part", \
+		"skills":"Unity3D / Vuforia", \
+		"cover":"img/works/arbasketball/1.jpg", \
+		"link":"", \
+		"video":"", \
+		"imgs":[ "img/works/arbasketball/1.jpg"] \
+		},\
+	"anicreator": {"title":"Animation Creator", \
+		"comment":"Machine learning based animation creator", \
+		"responsible":"Entire technical part", \
+		"skills":"Unity3D / Tensorflow / MotionCapture", \
+		"cover":"img/works/anicreator/1.jpg", \
+		"link":"", \
+		"video":"", \
+		"imgs":[ "img/works/anicreator/1.jpg"] \
+		},\
+	"depthmap": {"title":"DepthMap Generator", \
+		"comment":"Webcam based depthMap Generator", \
+		"responsible":"Entire technical part", \
+		"skills":"OpenCV / QT", \
+		"cover":"img/works/depthmap/1.jpg", \
+		"link":"", \
+		"video":"", \
+		"imgs":[ "img/works/depthmap/1.jpg"] \
+		},\
+	"xplane": {"title":"Realtime Flight Tracker 3D", \
+		"comment":"VR realtime flight tracking system using real flight tracking data", \
+		"responsible":"Entire technical part", \
+		"skills":"XPlane / VR / REST Api", \
+		"cover":"img/works/xplane/1.jpg", \
+		"link":"", \
+		"video":"", \
+		"imgs":[ "img/works/xplane/1.jpg"] \
+		},\
+	"room3d": {"title":"PickMe3D", \
+		"comment":"Furniture Layout & Space Allocation Simulator", \
+		"responsible":"Entire technical part", \
+		"skills":"OpenGL / QT / Web", \
+		"cover":"img/works/room3d/1.jpg", \
+		"link":"", \
+		"video":"", \
+		"imgs":[ "img/works/room3d/1.jpg"] \
+		},\
+	"masked face": {"title":"masked face VR", \
+		"comment":"We can make our face with masked", \
+		"responsible":"Entire technical part", \
+		"skills":"CG", \
+		"cover":"img/works/unity/AR/masked1.jpg", \
+		"link":"https://photos.app.goo.gl/zv9WPpYkf1Vv7Z3R8", \
+		"video":"", \
+		"imgs":[ "img/works/unity/AR/masked1.jpg", \
+				"img/works/unity/AR/masked2.jpg", \
+				"img/works/unity/AR/masked3.jpg"] \
+		},\
+	"objectdetect": {"title":"Realtime Object Detection", \
+		"comment":"Deep learning based realtime object detection program", \
+		"responsible":"Entire technical part", \
+		"skills":"Keras / Tensorflow", \
+		"cover":"img/works/objectdetect/1.jpg", \
+		"link":"", \
+		"video":"", \
+		"imgs":[ "img/works/objectdetect/1.jpg"] \
+		},\
+	"Counter strike": {"title":"Counter Strike VR", \
+		"comment":"We can enjoy counter strike in VR space", \
+		"responsible":"Entire technical part", \
+		"skills":"CG,Unity3D", \
+		"cover":"img/works/unity/VR/CS1.jpg", \
+		"link":"https://photos.app.goo.gl/FoCistAmAV6CWXmR9", \
+		"video":"", \
+		"imgs":[ "img/works/unity/VR/CS1.jpg", \
+				"img/works/unity/VR/CS2.jpg", \
+				"img/works/unity/VR/CS3.jpg"] \
+		},\
+	"satellite": {"title":"Satellite Visualization Application", \
+		"comment":"3D Visualization tool of satellites data", \
+		"responsible":"Entire front part", \
+		"skills":"OpenGL / SceneGraph/ QT", \
+		"cover":"img/works/satellite/1.jpg", \
+		"link":"", \
+		"video":"", \
+		"imgs":[ "img/works/satellite/1.jpg"] \
+		},\
+	"drivesim": {"title":"Car Driving Simulation", \
+		"comment":"3D simulation of driving a car with high quality rendering", \
+		"responsible":"Entire technical part", \
+		"skills":"Unreal Rendering / Depth Map / Semantics Map", \
+		"cover":"img/works/drivesim/1.jpg", \
+		"link":"", \
+		"video":"", \
+		"imgs":[ "img/works/drivesim/1.jpg"] \
+		},\
+	"arnavigation": {"title":"AR Navigation", \
+		"comment":"Augmented reality-powered car navigation system", \
+		"responsible":"Entire technical part", \
+		"skills":"OpenGL / SLAM / Scene Graph Management", \
+		"cover":"img/works/arnavigation/1.jpg", \
+		"link":"", \
+		"video":"", \
+		"imgs":[ "img/works/arnavigation/1.jpg"] \
+		},\
+	"gpdesign": {"title":"GP Designer", \
+		"comment":"General purpose 2D,3D design software", \
+		"responsible":"Entire technical part", \
+		"skills":"CAD Engine / OpenGL / MFC", \
+		"cover":"img/works/gpdesign/1.jpg", \
+		"link":"", \
+		"video":"", \
+		"imgs":[ "img/works/gpdesign/1.jpg"] \
+		},\
+	"Visiting AR": {"title":"Visiting AR", \
+		"comment":"We can go anywhere using AR", \
+		"responsible":"Entire technical part", \
+		"skills":"ARKit,Unity3D", \
+		"cover":"img/works/unity/AR/VisitingAR.jpg", \
+		"link":"https://photos.app.goo.gl/N5MkiXuicpCeNZ3n7", \
+		"video":"", \
+		"imgs":[ "img/works/unity/AR/VisitingAR.jpg"] \
+		},\
+	"DragonFight AR": {"title":"DragonFight AR", \
+		"comment":"We can see fighting with dragon using AR", \
+		"responsible":"Entire technical part", \
+		"skills":"ARKit,Unity3D", \
+		"cover":"img/works/unity/AR/dragonFight1.jpg", \
+		"link":"https://photos.app.goo.gl/etEkCMwNq5PTBitQ8", \
+		"video":"", \
+		"imgs":[ "img/works/unity/AR/dragonFight1.jpg", \
+				"img/works/unity/AR/dragonFight2.jpg", \
+				"img/works/unity/AR/dragonFight3.jpg", \
+				"img/works/unity/AR/dragonFight4.jpg"] \
+		},\
+	"arpeople": {"title":"AR based people finder", \
+		"comment":"People finder mobile app based on AR tech", \
+		"responsible":"Entire technical part", \
+		"skills":"ARKit / Vuforia", \
+		"cover":"img/works/arpeople/1.jpg", \
+		"link":"", \
+		"video":"", \
+		"imgs":[ "img/works/arpeople/1.jpg"] \
+		}\
+	}\
+}';
