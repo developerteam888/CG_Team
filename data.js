ff2d1de926e2b15fecfab86aa79688382e887766
solution888 <solution888.github.com>
2019-11-27 22:53:53 +0100

    fixed version

--- data.js
+++ data.js
@@ -11,17 +11,17 @@ let data = '{ \
 				"img/works/car_racing/2.jpg", \
 				"img/works/car_racing/3.jpg"] \
 		}, \
-	"animal_race": {"title":"Animal Racing Game", \
-		"comment":"An exciting go-kart simulation game with a twist", \
+	"SLAM AR": {"title":"AR based on SLAM", \
+		"comment":"This is an ar project based on SLAM.", \
 		"responsible":"Entire technical part", \
-		"skills":"Unity3D", \
-		"cover":"img/works/animal_racing/1.jpg", \
-		"link":"", \
-		"video":"./img/works/animal_racing/video.mp4", \
-		"imgs":[ "img/works/animal_racing/1.jpg", \
-				"img/works/animal_racing/2.jpg", \
-				"img/works/animal_racing/3.jpg", \
-				"img/works/animal_racing/4.jpg"] \
+		"skills":"SLAM,ARkit", \
+		"cover":"img/works/SLAM-AR/SLAM1.jpg", \
+		"link":"https://photos.app.goo.gl/bWtSs4uogHLMMzwx7", \
+		"video":"", \
+		"imgs":[ "img/SLAM-AR/SLAM1.jpg", \
+				"img/SLAM-AR/SLAM2.jpg", \
+				"img/SLAM-AR/SLAM3.jpg", \
+				"img/SLAM-AR/SLAM4.jpg"] \
 		}, \
 	"ski": {"title":"Go for the gold experience", \
 		"comment":"Ski jumping, Speed skating simulation game", \
